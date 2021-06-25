6a26d2731c14cb985f8010c0595b990c6a12eace
Emile <emile.swarts@digital.justice.gov.uk>
2021-06-25 13:38:44 +0100

    Pull task definition name from parameter store
    This is better than hardcoding strings with the names

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -4,14 +4,21 @@ set -euo pipefail
 
 source ./scripts/aws_helpers.sh
 
+assume_deploy_role() {
+  TEMP_ROLE=`aws sts assume-role --role-arn $ROLE_ARN --role-session-name ci-radius-deploy-$CODEBUILD_BUILD_NUMBER`
+  export AWS_ACCESS_KEY_ID=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.AccessKeyId')
+  export AWS_SECRET_ACCESS_KEY=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SecretAccessKey')
+  export AWS_SESSION_TOKEN=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SessionToken')
+}
+
 migrate() {
   local migration_command="./bin/rails db:migrate"
   local docker_service_name="admin"
   local cluster_name service_name task_definition docker_service_name
 
-  cluster_name="network-access-control-${ENV}-admin-cluster"
-  service_name="network-access-control-${ENV}-admin-service"
-  task_definition="network-access-control-${ENV}-admin-task"
+  cluster_name=$( jq -r '.admin.ecs.cluster_name' <<< "${NAC_TERRAFORM_OUTPUTS}" )
+  service_name=$( jq -r '.admin.ecs.service_name' <<< "${NAC_TERRAFORM_OUTPUTS}" )
+  task_definition=$( jq -r '.admin.ecs.task_definition_name' <<< "${NAC_TERRAFORM_OUTPUTS}" )
 
   aws sts get-caller-identity
 
@@ -22,7 +29,6 @@ migrate() {
     "${docker_service_name}" \
     "${migration_command}"
 }
-
 
 main() {
   assume_deploy_role
