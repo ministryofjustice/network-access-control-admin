ebc54984c6f9cf76186b4b93b8e58ca5b4cc4c08
csabagyorfi <csaba@madetech.com>
2021-06-25 09:34:00 +0100

    Remove AddDeviseTrackableToUsers migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_104412) do
+ActiveRecord::Schema.define(version: 2020_10_14_102047) do
   create_table "audits", charset: "utf8", force: :cascade do |t|
     t.integer "auditable_id"
     t.string "auditable_type"
