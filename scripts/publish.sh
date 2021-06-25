df9e5f44ddb60d82df9966bef1980e4039b67386
Emile <emile.swarts@digital.justice.gov.uk>
2021-06-25 12:11:06 +0100

    Reference repository url instead of name
    This will be used to publish to ECR

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-repository_name=$( jq -r '.admin.ecr.repository_name' <<< "${TERRAFORM_OUTPUTS}" )
+repository_url=$( jq -r '.admin.ecr.repository_url' <<< "${TERRAFORM_OUTPUTS}" )
 
 aws ecr get-login-password | docker login --username AWS --password-stdin ${REGISTRY_URL}
-docker tag admin:latest ${REGISTRY_URL}/${repository_name}
-docker push ${REGISTRY_URL}/${repository_name}:latest
+docker tag admin:latest ${repository_url}
+docker push ${repository_url}:latest
