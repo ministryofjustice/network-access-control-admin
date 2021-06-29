c55d1e8686710bb3b09a723538f71db77c851d58
csabagyorfi <csaba@madetech.com>
2021-06-29 12:09:36 +0100

    Reset schema.rb and add migration for users

---
+++ db/migrate/20210629091103_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.boolean :editor, default: false
+      t.string :email
+      t.timestamps null: false
+    end
+  end
+end
