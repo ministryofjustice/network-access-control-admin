c8b50fbe59747914f9eaef2b7b8dbaebc8c4e647
Yusuf <yusuf@madetech.com>
2021-07-07 14:35:53 +0100

    Add model tests for policies

---
+++ spec/factories/policy.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :policy do
+    sequence(:name) { |n| "Policy #{n}" }
+    sequence(:description) { |n| "Policy description #{n}" }
+  end
+end
---
+++ spec/models/policy_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe Policy, type: :model do
+  subject { build :policy }
+
+  it "has a valid policy" do
+    expect(subject).to be_valid
+  end
+
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :description }
+  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+end
