68314ce2a5e577160bfef28e327f9d674ab97eac
Yusuf <yusuf@madetech.com>
2021-07-07 14:50:34 +0100

    Add viewing of policies

---
+++ spec/acceptance/policy/view_policies_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+describe "showing a policy", type: :feature do
+  context "when the user is unauthenticated" do
+    it "does not allow viewing policies" do
+      visit "/policies/nonexistant-policy-id"
+
+      expect(page).to have_content "You need to sign in or sign up before continuing."
+    end
+  end
+
+  context "when the user is authenticated" do
+    before do
+      login_as create(:user, :reader)
+    end
+
+    context "when the policy exists" do
+      let!(:policy) { create :policy }
+
+      it "allows viewing policies" do
+        visit "/policies"
+
+        click_on "View", match: :first
+
+        expect(page).to have_content policy.name
+        expect(page).to have_content policy.description
+      end
+    end
+  end
+end
