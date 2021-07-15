db13c806b0768208821e4386dccce1f8f2c56f48
Yusuf <yusuf@madetech.com>
2021-07-15 14:40:17 +0100

    Add deletion of MAC authentication bypasses
    Co-authored-by: Emile <emile@madetech.com>

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     match "sign_out", to: "devise/sessions#destroy", as: :destroy_user_session, via: %i[get delete]
   end
 
-  resources :mac_authentication_bypasses, only: %i[index new create]
+  resources :mac_authentication_bypasses, only: %i[index new create destroy]
   resources :sites, :vlans
   resources :policies do
     resources :rules
