029b68e9f06d73b01555a2b43af96a57e252fde8
csabagyorfi <csaba@madetech.com>
2021-07-15 16:10:03 +0100

    Create certificate model and factory
    Co-authored-by: Michael Cullen <michael.cullen@madetech.com>

---
+++ app/models/certificate.rb
@@ -0,0 +1,6 @@
+class Certificate < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates_presence_of :description, :expiry_date, :properties
+
+  audited
+end
