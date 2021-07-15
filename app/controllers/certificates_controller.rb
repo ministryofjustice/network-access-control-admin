f4adc2ef7f408a20e621a61fa4b5694185631afd
Rubocop <runner@fv-az397-446.digkgshymtuujfwfpq2czj2vug.jx.internal.cloudapp.net>
2021-07-15 15:54:12 +0000

    Commit changes made by Rubocop

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -1,14 +1,13 @@
 class CertificatesController < ApplicationController
-  def index
-  end
+  def index; end
 
   def new
     @certificate = Certificate.new
   end
 
   def create
-    # TODO Create a use-case to read expiry_date and metadata from file
-    # and merge with the :name and :description params, something like: 
+    # TODO: Create a use-case to read expiry_date and metadata from file
+    # and merge with the :name and :description params, something like:
     # certificate_metadata = UseCases::ReadCertificate(file).execute
     @certificate = Certificate.new(certificate_params)
 
