68f0b934a11c91ffeb09f81130c6f25ee71a1844
csabagyorfi <csaba@madetech.com>
2021-07-16 11:31:25 +0100

    Read certificate metadata
    Next step: handle errors when certificate is not present or incorrect file format

--- spec/models/certificate_spec.rb
+++ spec/models/certificate_spec.rb
@@ -10,6 +10,6 @@ describe Certificate, type: :model do
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_presence_of :description }
   it { is_expected.to validate_presence_of :expiry_date }
-  it { is_expected.to validate_presence_of :properties }
+  it { is_expected.to validate_presence_of :subject }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
 end
