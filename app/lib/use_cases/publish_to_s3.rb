1ec4f59bc051ffc0bf52e76ec1d2d7801e2e69c9
Emile Swarts <emile.swarts123@gmail.com>
2021-07-16 10:26:05 +0100

    Ability to publish files to S3 (#35)
    * Ability to publish files to S3
    
    This introduces the usecase to publish files of any type to S3.
    Can be used for certificates or FreeRadius configuration files.
    
    * Commit changes made by Rubocop
    
    Co-authored-by: Rubocop <runner@fv-az397-446.digkgshymtuujfwfpq2czj2vug.jx.internal.cloudapp.net>

---
+++ app/lib/use_cases/publish_to_s3.rb
@@ -0,0 +1,13 @@
+class UseCases::PublishToS3
+  def initialize(destination_gateway:)
+    @destination_gateway = destination_gateway
+  end
+
+  def call(config)
+    destination_gateway.write(data: config)
+  end
+
+private
+
+  attr_reader :destination_gateway
+end
