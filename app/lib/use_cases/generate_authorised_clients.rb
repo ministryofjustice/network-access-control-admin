da8e500df77322c4e0bd8b489d9429c4119d8366
Yusuf <yusuf@madetech.com>
2021-07-16 11:50:34 +0100

    Refactor: Apply rubocop fixes

--- app/lib/use_cases/generate_authorised_clients.rb
+++ app/lib/use_cases/generate_authorised_clients.rb
@@ -1,7 +1,5 @@
 class UseCases::GenerateAuthorisedClients
   def call(mac_authentication_bypasses:)
-    mac_authentication_bypasses.map { |mab|
-      mab.address
-    }.join("\n")
+    mac_authentication_bypasses.map(&:address).join("\n")
   end
 end
