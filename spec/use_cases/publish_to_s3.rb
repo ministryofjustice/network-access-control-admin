8637b499bca5291416d4a94626ef329d89a86047
Emile <emile.swarts@digital.justice.gov.uk>
2021-07-16 10:18:23 +0100

    Ability to publish files to S3
    This introduces the usecase to publish files of any type to S3.
    Can be used for certificates or FreeRadius configuration files.

---
+++ spec/use_cases/publish_to_s3.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe UseCases::PublishToS3 do
+  subject(:use_case) do
+    described_class.new(
+      destination_gateway: s3_gateway
+    )
+  end
+
+  let(:s3_gateway) { instance_spy(Gateways::S3) }
+  let(:config) { "any file contents" }
+
+  before do
+    use_case.call(config)
+  end
+
+  it "publishes the Authorised Macs" do
+    expect(s3_gateway).to have_received(:write)
+      .with(data: config)
+  end
+end
