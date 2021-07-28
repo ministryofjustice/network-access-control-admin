768322f5375ef53154c5ccb82fefbbd7b9498f21
csabagyorfi <csaba@madetech.com>
2021-07-28 14:49:16 +0100

    Not to show list of clients when a site has no clients

--- spec/acceptance/clients/show_clients_spec.rb
+++ spec/acceptance/clients/show_clients_spec.rb
@@ -23,9 +23,20 @@ describe "showing a client", type: :feature do
       it "allows viewing clients" do
         visit "/sites/#{site.id}"
 
+        expect(page).to have_content "List of Authorised Clients"
         expect(page).to have_content client.tag
         expect(page).to have_content client.ip_range
         expect(page).to have_content client.shared_secret
+      end
+    end
+
+    context "when a site has no clients" do
+      let!(:site) { create :site }
+
+      it "allows viewing clients" do
+        visit "/sites/#{site.id}"
+
+        expect(page).to_not have_content "List of Authorised Clients"
       end
     end
   end
