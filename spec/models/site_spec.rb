a6ba297792454ffa5156753771509479a2a89e4f
csabagyorfi <csaba@madetech.com>
2021-07-28 14:43:07 +0100

    Update site model

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -9,4 +9,5 @@ RSpec.describe Site, type: :model do
 
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  it { is_expected.to have_many(:clients) }
 end
