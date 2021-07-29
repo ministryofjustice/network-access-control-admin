035505fed8ba08c83101a03fa76e553f5e5ba0a6
Yusuf <yusuf@madetech.com>
2021-07-29 12:31:37 +0100

    Add validation for client IP range
    Co-authored-by: Csaba <csaba@madetech.com>

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,6 +2,15 @@ class Client < ApplicationRecord
   belongs_to :site
 
   validates_presence_of :tag, :ip_range, :shared_secret
+  validate :validate_ip, on: :create
+
+private
+
+  def validate_ip
+    return if ip_range.blank?
+
+    errors.add(:ip_range, "is invalid") unless IPAddress.valid?(ip_range)
+  end
 
   audited
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -11,4 +11,27 @@ describe Client, type: :model do
   it { is_expected.to validate_presence_of :tag }
   it { is_expected.to validate_presence_of :shared_secret }
   it { is_expected.to validate_presence_of :ip_range }
+
+  it "has a valid IP range" do
+    valid_ip_addresses = %i[
+      10.0.0.1/32
+      10.0.0.2/24
+    ]
+
+    invalid_ip_addresses = %i[
+      300.0.0.1/32
+      300.0.0.2/100
+      invalid
+    ]
+
+    valid_ip_addresses.each do |ip|
+      result = build(:client, ip_range: ip)
+      expect(result).to be_valid
+    end
+
+    invalid_ip_addresses.each do |ip|
+      result = build(:client, ip_range: ip)
+      expect(result).to be_invalid
+    end
+  end
 end
