c9d72828a1726dac452f7280c2f2386e570c4b47
csabagyorfi <csaba@madetech.com>
2021-07-30 14:55:49 +0100

    Update generated client config format
     Co-authored-by: Yusuf Sheikh <yusuf@madetech.com>

--- app/lib/use_cases/generate_authorised_clients.rb
+++ app/lib/use_cases/generate_authorised_clients.rb
@@ -2,9 +2,9 @@ class UseCases::GenerateAuthorisedClients
   def call(clients:)
     output = clients.map do |client|
       if IPAddress.valid_ipv4_subnet?(client.ip_range)
-        "client #{client.tag.sub(" ", "_").downcase} {\n\tipv4addr = #{client.ip_range}\n\tsecret = #{client.shared_secret}\n}"
+        "client #{client.ip_range} {\n\tipv4addr = #{client.ip_range}\n\tsecret = #{client.shared_secret}\n\tshortname = #{client.tag}\n}"
       else
-        "client #{client.tag.sub(" ", "_").downcase} {\n\tipv6addr = #{client.ip_range}\n\tsecret = #{client.shared_secret}\n}"
+        "client #{client.ip_range} {\n\tipv6addr = #{client.ip_range}\n\tsecret = #{client.shared_secret}\n\tshortname = #{client.tag}\n}"
       end
     end
 
--- spec/use_cases/generate_authorised_clients_spec.rb
+++ spec/use_cases/generate_authorised_clients_spec.rb
@@ -1,26 +1,28 @@
 require "rails_helper"
 
 describe UseCases::GenerateAuthorisedClients do
-	subject(:result) do
-		described_class.new.call(clients: clients)
-	end
+  subject(:result) do
+    described_class.new.call(clients: clients)
+  end
 
-	describe "#call" do
-		describe "when there are entries in the database" do
+  describe "#call" do
+    describe "when there are entries in the database" do
       context "and the IP ranges are IPv4" do
-			let!(:first_ipv4_client) { create(:client, ip_range: "123.123.0.1/24") }
-			let!(:second_ipv4_client) { create(:client, ip_range: "123.123.0.2/32") }
-			let(:clients) { Client.all }
+        let!(:first_ipv4_client) { create(:client, ip_range: "123.123.0.1/24") }
+        let!(:second_ipv4_client) { create(:client, ip_range: "123.123.0.2/32") }
+        let(:clients) { Client.all }
 
         it "generates an authorised clients configuration file" do
-          expected_config = "client #{first_ipv4_client.tag.sub(" ", "_").downcase} {
-\tipv4addr = 123.123.0.1/24
+          expected_config = "client #{first_ipv4_client.ip_range} {
+\tipv4addr = #{first_ipv4_client.ip_range}
 \tsecret = #{first_ipv4_client.shared_secret}
+\tshortname = #{first_ipv4_client.tag}
 }
 
-client #{second_ipv4_client.tag.sub(" ", "_").downcase} {
+client #{second_ipv4_client.ip_range} {
 \tipv4addr = 123.123.0.2/32
 \tsecret = #{second_ipv4_client.shared_secret}
+\tshortname = #{second_ipv4_client.tag}
 }"
 
           expect(result).to eq(expected_config)
@@ -28,32 +30,34 @@ client #{second_ipv4_client.tag.sub(" ", "_").downcase} {
       end
 
       context "and the IP ranges are IPv6" do
-			  let!(:first_ipv6_client) { create(:client, ip_range: "2001:0db8:85a3:0000:0000:8a2e:0370:7334/32") }
-			  let!(:second_ipv6_client) { create(:client, ip_range: "2002::1/128") }
-  			let(:clients) { Client.all }
+        let!(:first_ipv6_client) { create(:client, ip_range: "2001:0db8:85a3:0000:0000:8a2e:0370:7334/32") }
+        let!(:second_ipv6_client) { create(:client, ip_range: "2002::1/128") }
+        let(:clients) { Client.all }
 
         it "generates an authorised clients configuration file" do
-          expected_config = "client #{first_ipv6_client.tag.sub(" ", "_").downcase} {
-\tipv6addr = 2001:0db8:85a3:0000:0000:8a2e:0370:7334/32
+          expected_config = "client #{first_ipv6_client.ip_range} {
+\tipv6addr = #{first_ipv6_client.ip_range}
 \tsecret = #{first_ipv6_client.shared_secret}
+\tshortname = #{first_ipv6_client.tag}
 }
 
-client #{second_ipv6_client.tag.sub(" ", "_").downcase} {
-\tipv6addr = 2002::1/128
+client #{second_ipv6_client.ip_range} {
+\tipv6addr = #{second_ipv6_client.ip_range}
 \tsecret = #{second_ipv6_client.shared_secret}
+\tshortname = #{second_ipv6_client.tag}
 }"
 
           expect(result).to eq(expected_config)
         end
       end
-		end
+    end
 
-		describe "when there are no entries in the database" do
-		  let(:clients) { [] }
+    describe "when there are no entries in the database" do
+      let(:clients) { [] }
 
-		  it "generates an empty authorised clients configuration file" do
-		    expect(result).to eq("")
-		  end
-		end
-	end
+      it "generates an empty authorised clients configuration file" do
+        expect(result).to eq("")
+      end
+    end
+  end
 end
