c9e1b2009ab02437916c4181afce1c4821f0188d
Yusuf <yusuf@madetech.com>
2021-07-30 11:09:42 +0100

    Add client config. for when IP ranges are IPv6

--- spec/use_cases/generate_authorised_clients_spec.rb
+++ spec/use_cases/generate_authorised_clients_spec.rb
@@ -7,23 +7,45 @@ describe UseCases::GenerateAuthorisedClients do
 
 	describe "#call" do
 		describe "when there are entries in the database" do
-			let(:first_client) { create(:client, ip_range: "123.123.0.1/24") }
-			let(:second_client) { create(:client, ip_range: "123.123.0.2/32") }
+      context "and the IP ranges are IPv4" do
+			let(:first_ipv4_client) { create(:client, ip_range: "123.123.0.1/24") }
+			let(:second_ipv4_client) { create(:client, ip_range: "123.123.0.2/32") }
 			let(:clients) { Client.all }
 
-			it "generates a authorised_clients configuration file" do
-				expected_config = "client #{first_client.tag.sub(" ", "_").downcase} {
+        it "generates a authorised_clients configuration file" do
+          expected_config = "client #{first_ipv4_client.tag.sub(" ", "_").downcase} {
 \tipv4addr = 123.123.0.1/24
-\tsecret = #{first_client.shared_secret}
+\tsecret = #{first_ipv4_client.shared_secret}
 }
 
-client #{second_client.tag.sub(" ", "_").downcase} {
+client #{second_ipv4_client.tag.sub(" ", "_").downcase} {
 \tipv4addr = 123.123.0.2/32
-\tsecret = #{second_client.shared_secret}
+\tsecret = #{second_ipv4_client.shared_secret}
 }"
 
-				expect(result).to include(expected_config)
-			end
+          expect(result).to include(expected_config)
+        end
+      end
+
+      context "and the IP ranges are IPv6" do
+			  let(:first_ipv6_client) { create(:client, ip_range: "2001:0db8:85a3:0000:0000:8a2e:0370:7334/32") }
+			  let(:second_ipv6_client) { create(:client, ip_range: "2002::1/128") }
+  			let(:clients) { Client.all }
+
+        it "generates a authorised_clients configuration file" do
+          expected_config = "client #{first_ipv6_client.tag.sub(" ", "_").downcase} {
+\tipv6addr = 2001:0db8:85a3:0000:0000:8a2e:0370:7334/32
+\tsecret = #{first_ipv6_client.shared_secret}
+}
+
+client #{second_ipv6_client.tag.sub(" ", "_").downcase} {
+\tipv6addr = 2002::1/128
+\tsecret = #{second_ipv6_client.shared_secret}
+}"
+
+          expect(result).to include(expected_config)
+        end
+      end
 		end
 
 		describe "when there are no entries in the database" do
