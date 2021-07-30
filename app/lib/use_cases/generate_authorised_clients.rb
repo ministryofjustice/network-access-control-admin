c20765e2abc75655225ab784c9d0729a7ecaf375
Yusuf <yusuf@madetech.com>
2021-07-30 11:09:42 +0100

    Add client config. for when IP ranges are IPv6

--- app/lib/use_cases/generate_authorised_clients.rb
+++ app/lib/use_cases/generate_authorised_clients.rb
@@ -1,7 +1,11 @@
 class UseCases::GenerateAuthorisedClients
   def call(clients:)
     output = clients.map do |client|
-      "client #{client.tag.sub(" ", "_").downcase} {\n\tipv4addr = #{client.ip_range}\n\tsecret = #{client.shared_secret}\n}"
+      if IPAddress.valid_ipv6_subnet? client.ip_range
+        "client #{client.tag.sub(" ", "_").downcase} {\n\tipv6addr = #{client.ip_range}\n\tsecret = #{client.shared_secret}\n}"
+      else
+        "client #{client.tag.sub(" ", "_").downcase} {\n\tipv4addr = #{client.ip_range}\n\tsecret = #{client.shared_secret}\n}"
+      end
     end
 
     output.join("\n\n")
