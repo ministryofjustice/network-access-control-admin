18531d0c37d775f108ddd1f000eca8ef3128d2ad
csabagyorfi <csaba@madetech.com>
2021-07-30 14:24:00 +0100

    Refactor: Rename tests in generate_authorised_clients_spec
     Co-authored-by: Yusuf Sheikh <yusuf@madetech.com>

--- spec/use_cases/generate_authorised_clients_spec.rb
+++ spec/use_cases/generate_authorised_clients_spec.rb
@@ -12,7 +12,7 @@ describe UseCases::GenerateAuthorisedClients do
 			let!(:second_ipv4_client) { create(:client, ip_range: "123.123.0.2/32") }
 			let(:clients) { Client.all }
 
-        it "generates a authorised_clients configuration file" do
+        it "generates an authorised clients configuration file" do
           expected_config = "client #{first_ipv4_client.tag.sub(" ", "_").downcase} {
 \tipv4addr = 123.123.0.1/24
 \tsecret = #{first_ipv4_client.shared_secret}
@@ -32,7 +32,7 @@ client #{second_ipv4_client.tag.sub(" ", "_").downcase} {
 			  let!(:second_ipv6_client) { create(:client, ip_range: "2002::1/128") }
   			let(:clients) { Client.all }
 
-        it "generates a authorised_clients configuration file" do
+        it "generates an authorised clients configuration file" do
           expected_config = "client #{first_ipv6_client.tag.sub(" ", "_").downcase} {
 \tipv6addr = 2001:0db8:85a3:0000:0000:8a2e:0370:7334/32
 \tsecret = #{first_ipv6_client.shared_secret}
@@ -51,7 +51,7 @@ client #{second_ipv6_client.tag.sub(" ", "_").downcase} {
 		describe "when there are no entries in the database" do
 		  let(:clients) { [] }
 
-		  it "generates an empty authorised_clients configuration file" do
+		  it "generates an empty authorised clients configuration file" do
 		    expect(result).to eq("")
 		  end
 		end
