548000a869fb076b0a9c2e7cdd446cafa40ad7ab
Yusuf <yusuf@madetech.com>
2021-07-30 15:19:30 +0100

    Refactor: Extract method for client definition
    - when generating a client config. file
    
    Co-authored-by: Csaba <csaba@madetech.com>

--- app/lib/use_cases/generate_authorised_clients.rb
+++ app/lib/use_cases/generate_authorised_clients.rb
@@ -1,13 +1,19 @@
 class UseCases::GenerateAuthorisedClients
   def call(clients:)
     output = clients.map do |client|
-      if IPAddress.valid_ipv4_subnet?(client.ip_range)
-        "client #{client.ip_range} {\n\tipv4addr = #{client.ip_range}\n\tsecret = #{client.shared_secret}\n\tshortname = #{client.tag}\n}"
-      else
-        "client #{client.ip_range} {\n\tipv6addr = #{client.ip_range}\n\tsecret = #{client.shared_secret}\n\tshortname = #{client.tag}\n}"
-      end
+      client_definition(client)
     end
 
     output.join("\n\n")
+  end
+
+private
+
+  def client_definition(client)
+    "client #{client.ip_range} {\n\t#{ip_version(client.ip_range)} = #{client.ip_range}\n\tsecret = #{client.shared_secret}\n\tshortname = #{client.tag}\n}"
+  end
+
+  def ip_version(ip_range)
+    IPAddress.valid_ipv4_subnet?(ip_range) ? "ipv4addr" : "ipv6addr"
   end
 end
