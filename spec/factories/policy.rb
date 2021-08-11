0faf7fd9ddff52c9ce65e6d7699c2bf12944d713
csabagyorfi <csaba@madetech.com>
2021-08-11 16:24:31 +0100

    Add fallback field to policies
    Co-authored-by: Yusuf Sheikh <yusuf@madetech.com>

--- spec/factories/policy.rb
+++ spec/factories/policy.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :policy do
     sequence(:name) { |n| "Policy #{n}" }
     sequence(:description) { |n| "Policy description #{n}" }
+    fallback { false }
   end
 end
