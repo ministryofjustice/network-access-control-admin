b81a5438c9a5e639e3046884b3ea2e48e4b81c09
Yusuf <yusuf@madetech.com>
2021-08-11 12:21:24 +0100

    Update site model to have many policies
    Co-authored-by: Csaba <csaba@madetech.com>

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,6 +2,7 @@ class Site < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 
   has_many :clients, dependent: :destroy
+  has_many :policies
 
   audited
 end
