48412b99adad869641f32af17231d6504e957648
csabagyorfi <csaba@madetech.com>
2021-08-13 16:01:01 +0100

    Validate the number of fallback policies per site

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,8 +1,15 @@
 class Site < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validate :validate_number_of_fallback_policies
 
   has_many :clients, dependent: :destroy
   has_and_belongs_to_many :policies, -> { distinct }
+
+private
+
+  def validate_number_of_fallback_policies
+    errors.add(:policies, "can only have one fallback policy") if policies.select(&:fallback).count > 1
+  end
 
   audited
 end
