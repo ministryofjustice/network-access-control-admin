c55bda7b86efbf588d1cf121345762fd42e1061e
csabagyorfi <csaba@madetech.com>
2021-08-18 16:10:53 +0100

    Clean up policy_response

--- app/models/policy_response.rb
+++ app/models/policy_response.rb
@@ -1,7 +1,3 @@
 class PolicyResponse < Response
   belongs_to :policy
-
-  def policy
-    Policy.find_by_id(policy_id)
-  end
 end
--- spec/models/policy_response_spec.rb
+++ spec/models/policy_response_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe PolicyResponse, type: :model do
-  subject { build :policy_response, policy: create(:policy) }
+  subject { build :policy_response }
 
   it "has a valid response" do
     expect(subject).to be_valid
@@ -10,8 +10,4 @@ describe PolicyResponse, type: :model do
   it { is_expected.to belong_to :policy }
   it { is_expected.to validate_presence_of :response_attribute }
   it { is_expected.to validate_presence_of :value }
-
-  it "has a policy" do
-    expect(subject.policy).to_not be_nil
-  end
 end
