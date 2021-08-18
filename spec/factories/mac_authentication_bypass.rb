f455438c290376cd0decf6bdfc4e8bdc9552e856
csabagyorfi <csaba@madetech.com>
2021-08-18 16:27:21 +0100

    Fix flaky mac_authentication_bypass factory
    We are using the record id to generate the last two digits of a mac
    address, however this breaks when the id is double digits. Use a hard
    coded value instead to avoid flaky tests.

--- spec/factories/mac_authentication_bypass.rb
+++ spec/factories/mac_authentication_bypass.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :mac_authentication_bypass do
-    sequence(:address) { |n| "aa-11-22-33-44-#{n}#{n}" }
+    sequence(:address) { "aa-11-22-33-44-11" }
     sequence(:name) { |n| "Printer #{n}" }
     sequence(:description) { |n| "Printer description #{n}" }
   end
