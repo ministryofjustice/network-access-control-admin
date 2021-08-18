edbdfbe3b8f775c8b72c65ca2bfb55c15eeaeb84
csabagyorfi <csaba@madetech.com>
2021-08-18 16:28:55 +0100

    Temp fix response_presenter
    We will need to implement some logic to handle
    mac_authentication_bypass_responses

--- app/presenters/mac_authentication_bypass_response_presenter.rb
+++
@@ -1,5 +0,0 @@
-class MacAuthenticationBypassResponsePresenter < BasePresenter
-  def display_name
-    "#{record.response_attribute}-#{record.value}, MAB: #{record.mac_authentication_bypass.name}"
-  end
-end
--- app/presenters/response_presenter.rb
+++ app/presenters/response_presenter.rb
@@ -1,5 +1,5 @@
 class ResponsePresenter < BasePresenter
   def display_name
-    "#{record.response_attribute}-#{record.value}, Policy: #{record.policy.name}"
+    "#{record.response_attribute}-#{record.value}, Policy: #{Policy.find_by_id(record.policy_id).name}"
   end
 end
