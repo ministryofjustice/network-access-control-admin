d60abb32158ef025749a1504154bd29d54413db3
Rubocop <runner@fv-az225-281.hxhba2eg53duheooqg2bzplbvd.bx.internal.cloudapp.net>
2021-08-20 13:59:18 +0000

    Commit changes made by Rubocop

--- spec/use_cases/generate_authorised_clients_spec.rb
+++ spec/use_cases/generate_authorised_clients_spec.rb
@@ -10,20 +10,20 @@ describe UseCases::GenerateAuthorisedClients do
       context "and the IP ranges are IPv4" do
         let!(:site) { create(:site, name: "Another Site") }
         let!(:first_ipv4_client) { create(:client, ip_range: "123.123.0.1/24", site_id: site.id) }
-        let!(:second_ipv4_client) { create(:client, ip_range: "123.123.0.2/32", site_id: site.id ) }
+        let!(:second_ipv4_client) { create(:client, ip_range: "123.123.0.2/32", site_id: site.id) }
         let(:clients) { Client.all }
 
         it "generates an authorised clients configuration file" do
           expected_config = "client #{first_ipv4_client.ip_range} {
 \tipv4addr = #{first_ipv4_client.ip_range}
 \tsecret = #{first_ipv4_client.shared_secret}
-\tshortname = #{site.name.parameterize(separator: "_")}
+\tshortname = #{site.name.parameterize(separator: '_')}
 }
 
 client #{second_ipv4_client.ip_range} {
 \tipv4addr = 123.123.0.2/32
 \tsecret = #{second_ipv4_client.shared_secret}
-\tshortname = #{site.name.parameterize(separator: "_")}
+\tshortname = #{site.name.parameterize(separator: '_')}
 }"
 
           expect(result).to eq(expected_config)
