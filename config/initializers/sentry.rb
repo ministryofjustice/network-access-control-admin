e37351ca16305d5ba31e039cc858503135e454d9
Emile <emile.swarts@digital.justice.gov.uk>
2021-08-25 13:15:27 +0100

    Fix typo with initialising Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,9 +2,6 @@ if ENV.fetch("RACK_ENV") == "production"
   Sentry.init do |config|
     config.dsn = ENV.fetch("SENTRY_DSN")
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
-
-  # Set tracesSampleRate to 1.0 to capture 100%
-  # of transactions for performance monitoring.
-  # We recommend adjusting this value in production
-  config.traces_sample_rate = 0.5
+    config.traces_sample_rate = 0.5
+  end
 end
