d0544058098a1b6d6c8854a67d6561eed2e68a91
Emile <emile.swarts@digital.justice.gov.uk>
2021-08-25 12:58:01 +0100

    Simplify environment checking
    We will only initialise sentry for production environments

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,4 @@
-if %w[production].include?(ENV.fetch("RACK_ENV"))
+if ENV.fetch("RACK_ENV") == "production"
   Sentry.init do |config|
     config.dsn = ENV.fetch("SENTRY_DSN")
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
