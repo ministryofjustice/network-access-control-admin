da83f4e51bca2597a872fe201087aa007c9dff73
Emile <emile.swarts@digital.justice.gov.uk>
2021-08-25 11:42:40 +0100

    Only instantiate Sentry in production
    Exclude from tests and local development from reporting errors

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,9 +1,11 @@
-Sentry.init do |config|
-  config.dsn = ENV.fetch("SENTRY_DSN")
-  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+if %w[production].include?(ENV.fetch("RACK_ENV"))
+  Sentry.init do |config|
+    config.dsn = ENV.fetch("SENTRY_DSN")
+    config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
-  # Set tracesSampleRate to 1.0 to capture 100%
-  # of transactions for performance monitoring.
-  # We recommend adjusting this value in production
-  config.traces_sample_rate = 1.0
+    # Set tracesSampleRate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production
+    config.traces_sample_rate = 1.0
+  end
 end
