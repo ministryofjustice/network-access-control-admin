622294a9819bbaabb571d6347d59d5d341a07b63
Yusuf <yusuf@madetech.com>
2021-08-26 12:08:54 +0100

    Add breadcrumbs to Audit pages

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -1,9 +1,17 @@
 class AuditsController < ApplicationController
+  before_action :set_crumbs, only: %i[index show]
+
   def index
     @audits = Audit.order(created_at: "DESC").page params[:page]
   end
 
   def show
     @audit = Audit.find(params[:id])
+  end
+
+private
+
+  def set_crumbs
+    @navigation_crumbs = [["Home", root_path], ["Audit Log", audits_path]]
   end
 end
