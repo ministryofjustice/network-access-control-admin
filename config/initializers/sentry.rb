1d1288b6bb80d2152a0cd5ba246e1b4c15b7839f
Emile <emile.swarts@digital.justice.gov.uk>
2021-08-27 11:30:30 +0100

    Disable Sentry performance monitoring
    We only use Sentry for errors, setting the sample rate attempts to
    publish performance metrics. This gets rate limited, remove it.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,5 @@ if ENV.fetch("RACK_ENV") == "production"
   Sentry.init do |config|
     config.dsn = ENV.fetch("SENTRY_DSN")
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
-    config.traces_sample_rate = 0.5
   end
 end
