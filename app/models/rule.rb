e65f59f02079ed3f17b6e490b4d93e8b32299fef
Emile Swarts <emileswarts@emileswartsmbp.home>
2021-10-19 10:43:37 +0100

    Add update_rule_count
    Each time a rule is saved, it will update the policy rule count.
    This value is required by the policy engine, and we do not want
    to compute it at runtime each time a authentication happens for
    performance reasons.

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,8 +1,16 @@
 class Rule < ApplicationRecord
   belongs_to :policy
 
+  after_save :update_rule_count
+
   validates_presence_of :request_attribute, :operator, :value
   validates_inclusion_of :operator, in: %w[equals contains]
 
   audited
+
+  private
+
+  def update_rule_count
+    policy.update_attribute(:rule_count, policy.rules.count)
+  end
 end
