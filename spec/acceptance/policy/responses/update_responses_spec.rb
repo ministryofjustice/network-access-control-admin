cf0e525e2d23b475c9823beee7504aba2a9389e2
Yusuf <yusuf@madetech.com>
2021-10-21 15:04:46 +0100

    Add acceptance test for updating a custom response
    - policy responses
    
    Co-authored-by: Csaba <csaba@madetech.com>

--- spec/acceptance/policy/responses/update_responses_spec.rb
+++ spec/acceptance/policy/responses/update_responses_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 describe "update responses", type: :feature do
   let(:policy) { create(:policy) }
   let(:response) { create(:policy_response, policy: policy) }
+  let(:custom_response) { create(:policy_response, response_attribute: "Custom-Attribute", policy: policy) }
 
   context "when the user is unauthenticated" do
     it "does not allow updating responses" do
@@ -34,6 +35,7 @@ describe "update responses", type: :feature do
     before do
       login_as editor
       response
+      custom_response
     end
 
     it "does update an existing response" do
@@ -56,6 +58,26 @@ describe "update responses", type: :feature do
       expect(page).to have_content "5678"
 
       expect_audit_log_entry_for(editor.email, "update", "Response")
+    end
+
+    it "does update an existing custom response" do
+      visit "policies/#{policy.id}"
+
+      all(:link, "Edit")[1].click
+
+      expect(page).to have_field("custom-response-attribute", with: custom_response.response_attribute)
+      expect(page).to have_field("Value", with: custom_response.value)
+
+      fill_in "custom-response-attribute", with: "Updated-Custom-Attribute"
+      fill_in "Value", with: "LAN"
+
+      click_on "Update"
+
+      expect(current_path).to eq("/policies/#{policy.id}")
+
+      expect(page).to have_content("Successfully updated response.")
+      expect(page).to have_content "Updated-Custom-Attribute"
+      expect(page).to have_content "LAN"
     end
   end
 end
