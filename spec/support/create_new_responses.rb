bd844e6834936ec264b4f909a9bf356d65666b90
csabagyorfi <csaba@madetech.com>
2021-10-21 17:06:51 +0100

    Dry out create response specs

--- spec/support/create_new_responses.rb
+++ spec/support/create_new_responses.rb
@@ -1,4 +1,4 @@
-RSpec.shared_examples "responses" do |domain|
+RSpec.shared_examples "new response creation" do |domain, response|
   context "when the user is an editor" do
     let(:editor) { create(:user, :editor) }
 
@@ -10,15 +10,15 @@ RSpec.shared_examples "responses" do |domain|
       logout
     end
 
-    context "when there is an existing MAC authentication bypass" do
+    context "when there is an existing response owner" do
       let!(:created_domain) { create(domain) }
 
       it "creates a new response from dictionary dropdown" do
-        visit "/mac_authentication_bypasses/#{created_domain.id}"
+        visit "/#{domain.to_s.pluralize}/#{created_domain.id}"
 
         click_on "Add response"
 
-        # expect(page.current_path).to eq(new_mac_authentication_bypass_mab_response_path(mac_authentication_bypass_id: domain))
+        expect(page.current_path).to eq("/#{domain.to_s.pluralize}/#{created_domain.id}/#{response.to_s.pluralize}/new")
 
         select "Tunnel-Type", from: "response-attribute"
         fill_in "Value", with: "1234"
@@ -26,16 +26,16 @@ RSpec.shared_examples "responses" do |domain|
         click_on "Create"
 
         expect(page).to have_content("Successfully created response.")
-        # expect(page.current_path).to eq(mac_authentication_bypass_path(id: domain.id))
+        expect(page.current_path).to eq("/#{domain.to_s.pluralize}/#{created_domain.id}")
         expect_audit_log_entry_for(editor.email, "create", "Response")
       end
 
       it "creates a new custom response" do
-        visit "/mac_authentication_bypasses/#{created_domain.id}"
+        visit "/#{domain.to_s.pluralize}/#{created_domain.id}"
 
         click_on "Add response"
 
-        # expect(page.current_path).to eq(new_mac_authentication_bypass_mab_response_path(mac_authentication_bypass_id: domain))
+        expect(page.current_path).to eq("/#{domain.to_s.pluralize}/#{created_domain.id}/#{response.to_s.pluralize}/new")
 
         choose "Custom"
         fill_in "custom-response-attribute", with: "Custom-Tunnel-Type"
@@ -44,12 +44,12 @@ RSpec.shared_examples "responses" do |domain|
         click_on "Create"
 
         expect(page).to have_content("Successfully created response.")
-        # expect(page.current_path).to eq(mac_authentication_bypass_path(id: mac_authentication_bypass.id))
+        expect(page.current_path).to eq("/#{domain.to_s.pluralize}/#{created_domain.id}")
         expect_audit_log_entry_for(editor.email, "create", "Response")
       end
 
-      xit "displays an error if form cannot be submitted" do
-        visit new_mac_authentication_bypass_mab_response_path(mac_authentication_bypass_id: mac_authentication_bypass)
+      it "displays an error if form cannot be submitted" do
+        visit "/#{domain.to_s.pluralize}/#{created_domain.id}/#{response.to_s.pluralize}/new"
 
         click_on "Create"
 
