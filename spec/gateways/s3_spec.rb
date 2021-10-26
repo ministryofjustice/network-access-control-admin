9892e4608bcfc470d2c999d7bba69f3ac06bd0b8
Yusuf <yusuf@madetech.com>
2021-10-26 15:20:34 +0100

    Add list object keys method to S3 gateway
    Co-authored-by: Csaba <csaba@madetech.com>

--- spec/gateways/s3_spec.rb
+++ spec/gateways/s3_spec.rb
@@ -1,3 +1,5 @@
+require "rails_helper"
+
 describe Gateways::S3 do
   subject(:gateway) do
     described_class.new(bucket: bucket, key: key,
@@ -6,6 +8,7 @@ describe Gateways::S3 do
 
   let(:bucket) { "StubBucket" }
   let(:key) { "StubKey" }
+  let(:prefix) { "StubPrefix" }
   let(:data) { { blah: "foobar" }.to_json }
   let(:aws_config) do
     {
@@ -13,6 +16,9 @@ describe Gateways::S3 do
         get_object: lambda { |context|
           { body: "some data" } if context.params.fetch(:bucket) == bucket && context.params.fetch(:key) == key
         },
+        list_objects_v2: lambda { |context|
+          ["dictionary.one", "dictionary.two"] if context.params.fetch(:prefix) == prefix
+        }
       },
     }
   end
@@ -34,6 +40,13 @@ describe Gateways::S3 do
   context "when reading a file from the S3 bucket" do
     it "returns the contents of the file" do
       expect(gateway.read).to eq("some data")
+    end
+  end
+
+  context "when reading multiples files from the S3 bucket" do
+    it "returns a list of file names" do
+      list_objects = gateway.list_object_keys(prefix)
+      expect(list_objects).to eq(["dictionary.one", "dictionary.two"])
     end
   end
 end
