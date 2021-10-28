7cc0e224b80564a8fd7114a5a636726070cb9e51
Yusuf <yusuf@madetech.com>
2021-10-28 15:58:27 +0100

    Update Response model to validate attributes
    Co-authored-by: Csaba <csaba@madetech.com>

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,14 @@
 class Response < ApplicationRecord
   validates_presence_of :response_attribute, :value
+  validate :validate_radius_attributes, on: :create
 
   audited
+
+private
+
+  def validate_radius_attributes
+    return if response_attribute.blank?
+
+    errors.add(:response_attribute, "is invalid") unless File.read("app/helpers/radius_dictionary_attributes.txt").include?(response_attribute)
+  end
 end
