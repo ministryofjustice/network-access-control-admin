ab76f4458faf54f942f682dab9a9b06adc5b7b20
csabagyorfi <csaba@madetech.com>
2021-11-17 17:23:14 +0000

    Fix pluralization in delete site message

--- spec/acceptance/sites/delete_sites_spec.rb
+++ spec/acceptance/sites/delete_sites_spec.rb
@@ -30,7 +30,7 @@ describe "delete sites", type: :feature do
       expect(page).to have_content("Are you sure you want to delete this site?")
       expect(page).to have_content(site.name)
       expect(page).to have_content("#{site.clients.count} clients will be deleted.")
-      expect(page).to have_content("#{site.policies.count} policies will be detached.")
+      expect(page).to have_content("#{site.policies.count} policy will be detached.")
 
       click_on "Delete site"
 
