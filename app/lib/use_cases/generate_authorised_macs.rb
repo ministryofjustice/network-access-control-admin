d397900a6f26e0966d422df87bed5b053626ce31
MichaelCullenMadeTech <79979174+MichaelCullenMadeTech@users.noreply.github.com>
2021-11-17 16:21:09 +0000

    Update password to have no delimiters (#218)
    * Update password to have no delimiters
    
    * Commit changes made by Rubocop
    
    Co-authored-by: Rubocop <runner@fv-az198-2.gjkxsqtjg41etoza45cw35jkyb.bx.internal.cloudapp.net>

--- app/lib/use_cases/generate_authorised_macs.rb
+++ app/lib/use_cases/generate_authorised_macs.rb
@@ -2,11 +2,17 @@ class UseCases::GenerateAuthorisedMacs
   def call(mac_authentication_bypasses:)
     bypasses = mac_authentication_bypasses.map do |bypass|
       responses = bypass.responses.map { |response| [response.response_attribute, response.value].join(" = ") }.join(",\n        ")
-      bypass_address_line = "#{bypass.address} Cleartext-Password := #{bypass.address}"
+      bypass_address_line = "#{bypass.address} Cleartext-Password := #{normalised_password(bypass.address)}"
 
       bypass.responses.empty? ? bypass_address_line : [bypass_address_line, responses].join("\n        ")
     end
 
     "#{bypasses.join("\n\n")}\n"
+  end
+
+private
+
+  def normalised_password(mac_address)
+    mac_address.gsub("-", "")
   end
 end
