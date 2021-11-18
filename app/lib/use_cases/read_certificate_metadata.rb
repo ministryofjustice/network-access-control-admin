e91b8a6e8758087c4cd2908971b7ef4d525d226a
Yusuf <yusuf@madetech.com>
2021-11-18 16:03:39 +0000

    Update read certificate use case to add extensions

--- app/lib/use_cases/read_certificate_metadata.rb
+++ app/lib/use_cases/read_certificate_metadata.rb
@@ -9,11 +9,16 @@ module UseCases
     def call
       decoded_certificate = OpenSSL::X509::Certificate.new(certificate)
 
+      extensions = decoded_certificate.extensions.map do |extension|
+        "#{extension.oid}:#{" critical" if extension.critical?}\n\t#{extension.value}"
+      end
+
       {
         expiry_date: decoded_certificate.not_after.to_date,
         subject: decoded_certificate.subject.to_s,
         issuer: decoded_certificate.issuer.to_s,
         serial: decoded_certificate.serial.to_s,
+        extensions: extensions.join("\n\n")
       }
     rescue StandardError
       {}
