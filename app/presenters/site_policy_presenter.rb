c217a4833c9429d8a4a876240da3aaeb44a86fc6
Yusuf <yusuf@madetech.com>
2021-11-18 11:23:39 +0000

    Update audit log to show policy and site names

--- app/presenters/site_policy_presenter.rb
+++ app/presenters/site_policy_presenter.rb
@@ -1,5 +1,19 @@
 class SitePolicyPresenter < BasePresenter
   def display_name
-    "#{record.site_id} - #{record.policy_id} - #{record.priority}"
+    "#{site_relation} - #{policy_relation}"
+  end
+
+  private
+
+  def policy_relation
+    if record.policy_id
+      "Policy: #{Policy.find_by_id(record.policy_id).try(:name)}"
+    end
+  end
+
+  def site_relation
+    if record.site_id
+      "Site: #{Site.find_by_id(record.site_id).try(:name)}"
+    end
   end
 end
