a36339adda1d5caad107b1731ca54bb3bef08850
Emile Swarts <emile.swarts123@gmail.com>
2021-11-22 10:09:43 +0000

    Assign all RADIUS attributes to Rails.application.config.radius_attri… (#226)
    * Assign all RADIUS attributes to Rails.application.config.radius_attributes
    
    These will be referenced in a future PR to display in a type-ahead
    select box for the Policy Engine.
    
    This is a better user experience than a freeform text field.
    
    * Commit changes made by Rubocop
    
    Co-authored-by: Rubocop <runner@fv-az219-596.ylzdhobuz03udiqwozmpg25a0d.xx.internal.cloudapp.net>

--- app/lib/use_cases/fetch_radius_attributes.rb
+++ app/lib/use_cases/fetch_radius_attributes.rb
@@ -6,6 +6,13 @@ module UseCases
     end
 
     def call
+      fetch_dictionaries_from_s3
+      extracted_attributes
+    end
+
+  private
+
+    def fetch_dictionaries_from_s3
       files = gateway.list_object_keys("radius_dictionaries").contents
 
       files.each do |file_d|
@@ -14,7 +21,23 @@ module UseCases
       end
     end
 
-  private
+    def extracted_attributes
+      attribute_lines.map { |a| a.split(" ")[1] }
+    end
+
+    def attribute_lines
+      active_dictionaries.map { |d|
+        File.read("#{output}/#{d}").each_line.select { |l| l.match?(/ATTRIBUTE/) }
+      }.flatten!
+    end
+
+    def active_dictionaries
+      active_dictionary_lines.map { |match| match.split(" ")[1] }
+    end
+
+    def active_dictionary_lines
+      File.read("#{output}/dictionary").each_line.select { |l| l.match?(/^\$INCLUDE/) }
+    end
 
     attr_reader :gateway, :output
   end
---
+++ config/initializers/fetch_attributes.rb
@@ -0,0 +1,24 @@
+require_relative "../../app/lib/gateways/s3"
+require_relative "../../app/lib/use_cases/fetch_radius_attributes"
+
+s3_gateway = Gateways::S3.new(
+  bucket: ENV.fetch("RADIUS_CONFIG_BUCKET_NAME"),
+  key: nil,
+  aws_config: Rails.application.config.s3_aws_config,
+  content_type: "text/plain",
+)
+
+begin
+  pp "Fetching RADIUS dictionaries..."
+
+  attributes = UseCases::FetchRadiusAttributes.new(
+    gateway: s3_gateway,
+    output: "/usr/share/freeradius/",
+  ).call
+
+  Rails.application.config.radius_attributes = attributes
+rescue StandardError => e
+  pp "Failed to fetch RADIUS dictionaries with error: #{e}"
+
+  raise e
+end
