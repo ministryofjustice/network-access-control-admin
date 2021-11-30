087440076c537aa5c5cd2c1ea1941e1c0824f6f3
csabagyorfi <csaba@madetech.com>
2021-11-30 16:37:10 +0000

    Ordering MABs by site

--- spec/acceptance/mac_authentication_bypass/list_spec.rb
+++ spec/acceptance/mac_authentication_bypass/list_spec.rb
@@ -6,7 +6,7 @@ describe "showing a MAC authentication bypass", type: :feature do
   end
 
   context "when the MAC authentication bypasses exists" do
-    let!(:mac_authentication_bypass) { create :mac_authentication_bypass, site: create(:site) }
+    let!(:mac_authentication_bypass) { create :mac_authentication_bypass }
 
     it "allows viewing bypasses" do
       visit "/mac_authentication_bypasses"
@@ -16,7 +16,30 @@ describe "showing a MAC authentication bypass", type: :feature do
       expect(page).to have_content mac_authentication_bypass.description
       expect(page).to have_content date_format(mac_authentication_bypass.created_at)
       expect(page).to have_content date_format(mac_authentication_bypass.updated_at)
-      expect(page).to have_content mac_authentication_bypass.site.name
+    end
+  end
+
+  context "when the MAC authentication bypasses exists with sites" do
+    let!(:a_mab) { create :mac_authentication_bypass, address: "aa-11-22-33-44-11", site: create(:site, name: "AAA") }
+    let!(:b_mab) { create :mac_authentication_bypass, address: "aa-11-22-33-44-12", site: create(:site, name: "BBB") }
+
+    it "allows ordering bypasses by site names" do
+      visit "/mac_authentication_bypasses"
+
+      expect(page).to have_content a_mab.site.name
+      expect(page).to have_content b_mab.site.name
+
+      within ".govuk-grid-row" do
+        first(:link, "Site").click
+      end
+
+      expect(page.text).to match(/#{a_mab.name}.*#{b_mab.name}/)
+
+      within ".govuk-grid-row" do
+        first(:link, "Site").click
+      end
+
+      expect(page.text).to match(/#{b_mab.name}.*#{a_mab.name}/)
     end
   end
 end
