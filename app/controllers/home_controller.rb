7a0b36698950466afd722c345876ee90613b8bfa
Emile <emile.swarts@digital.justice.gov.uk>
2021-12-03 11:24:43 +0000

    Use summary list instead of table for homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
   def index
     @stats = [
-      { label: "Sites", count: Site.all.count },
+      { label: "Sites", count: Site.all.count, link: sites_path },
+      { label: "MAC Adresses", count: MacAuthenticationBypass.all.count, link: mac_authentication_bypasses_path },
+      { label: "Policies", count: Policy.all.count, link: policies_path },
+      { label: "Certificates", count: Certificate.all.count, link: certificates_path },
       { label: "Clients", count: Client.all.count },
-      { label: "MAC Adresses", count: MacAuthenticationBypass.all.count },
-      { label: "Policies", count: Policy.all.count },
       { label: "Rules", count: Rule.all.count },
       { label: "Responses", count: Response.all.count },
-      { label: "Certificates", count: Certificate.all.count },
     ]
   end
 end
