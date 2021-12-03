1d05606938ecc55b57574bb40b3bd1fb3b7f8bdd
Emile <emile.swarts@digital.justice.gov.uk>
2021-12-03 10:39:29 +0000

    Update homepage with interesting overview information
    This now displays an introduction, configuration statistics
    and a getting started link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,13 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @stats = [
+      { label: "Sites", count: Site.all.count },
+      { label: "Clients", count: Client.all.count },
+      { label: "MAC Adresses", count: MacAuthenticationBypass.all.count },
+      { label: "Policies", count: Policy.all.count },
+      { label: "Rules", count: Rule.all.count },
+      { label: "Responses", count: Response.all.count },
+      { label: "Certificates", count: Certificate.all.count }
+    ]
+  end
 end
