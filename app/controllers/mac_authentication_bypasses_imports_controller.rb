b1983f006979aae654403ffafa4b33cdaf7cd5e4
Yusuf <yusuf@madetech.com>
2021-12-20 15:29:35 +0000

    Deploy service when importing MABs
    - publish authorised MACs to S3
    
    Co-authored-by: Csaba <csaba@madetech.com>

--- app/controllers/mac_authentication_bypasses_imports_controller.rb
+++ app/controllers/mac_authentication_bypasses_imports_controller.rb
@@ -10,6 +10,8 @@ class MacAuthenticationBypassesImportsController < ApplicationController
     @mac_authentication_bypasses_import = MacAuthenticationBypassesImport.new(contents)
 
     if @mac_authentication_bypasses_import.save
+      publish_authorised_macs
+      deploy_service
       redirect_to mac_authentication_bypasses_path, notice: "Successfully imported bypasses"
     else
       render :new
@@ -20,5 +22,22 @@ private
 
   def mac_authentication_bypasses_import_params
     params.require(:mac_authentication_bypasses_import).permit(:bypasses)
+  end
+
+  def publish_authorised_macs
+    content = UseCases::GenerateAuthorisedMacs.new.call(mac_authentication_bypasses: MacAuthenticationBypass.all)
+
+    UseCases::PublishToS3.new(
+      config_validator: UseCases::ConfigValidator.new(
+        config_file_path: "/etc/raddb/mods-config/files/authorize",
+        content: content,
+      ),
+      destination_gateway: Gateways::S3.new(
+        bucket: ENV.fetch("RADIUS_CONFIG_BUCKET_NAME"),
+        key: "authorised_macs",
+        aws_config: Rails.application.config.s3_aws_config,
+        content_type: "text/plain",
+      ),
+    ).call(content)
   end
 end
