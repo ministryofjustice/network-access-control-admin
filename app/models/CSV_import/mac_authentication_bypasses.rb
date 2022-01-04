806391c1bf416f8c9bc680dc229572dd8a5c68f0
Yusuf <yusuf@madetech.com>
2022-01-04 13:08:58 +0000

    Add use case for validating RADIUS attributes
    Co-authored-by: Csaba <csaba@madetech.com>

--- app/models/CSV_import/mac_authentication_bypasses.rb
+++ app/models/CSV_import/mac_authentication_bypasses.rb
@@ -115,23 +115,7 @@ module CSVImport
     end
 
     def validate_radius_attributes
-      content = UseCases::GenerateAuthorisedMacs.new.call(mac_authentication_bypasses: @records)
-
-      radius_errors = ""
-      UseCases::ConfigValidator.new(
-        config_file_path: "/etc/raddb/mods-config/files/authorize",
-        content: content,
-      ).call do |error|
-        radius_errors = error
-      end
-
-      @records.each_with_index do |record, i|
-        error_line = radius_errors.split("\n").select { |l| l.include?(record.address) }[0]
-        if error_line
-          message = error_line.match(/#{record.address}: (.*)/)[1]
-          errors.add(:base, "Error on row #{i + 2}: #{message}")
-        end
-      end
+      UseCases::ValidateRadiusAttributes.new(records: @records, errors: errors).call
     end
 
     def unwrap_responses(responses)
