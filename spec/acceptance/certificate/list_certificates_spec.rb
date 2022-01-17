c0837c7259c8b7c342c8910b5d74760f12f72033
Yusuf <yusuf@madetech.com>
2022-01-17 10:43:06 +0000

    Add ordering by name on Certificates page
    Co-authored-by: Csaba <csaba@madetech.com>

--- spec/acceptance/certificate/list_certificates_spec.rb
+++ spec/acceptance/certificate/list_certificates_spec.rb
@@ -16,5 +16,23 @@ describe "showing a certificate", type: :feature do
       expect(page).to have_content certificate.category
       expect(page).to have_content date_format(certificate.created_at)
     end
+
+    context "when ordering" do
+      let!(:first_certificate) { create(:certificate, name: "AA Certificate") }
+      let!(:second_certificate) { create(:certificate, name: "BB Certificate") }
+      let!(:third_certificate) { create(:certificate, name: "CC Certificate") }
+
+      before do
+        visit "/certificates"
+      end
+
+      it "orders by name" do
+        click_on "Name"
+        expect(page.text).to match(/AA Certificate.*BB Certificate/)
+
+        click_on "Name"
+        expect(page.text).to match(/BB Certificate.*AA Certificate/)
+      end
+    end
   end
 end
