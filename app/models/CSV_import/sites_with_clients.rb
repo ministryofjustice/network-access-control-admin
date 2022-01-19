5c3eee08f14628a2c7343a84376eb01f59ea13da
Yusuf <yusuf@madetech.com>
2022-01-19 15:56:08 +0000

    Add CSV import model for sites/client
    Implement happy path of importing Site and Client
    
    Co-authored-by: Csaba <csaba@madetech.com>

---
+++ app/models/CSV_import/sites_with_clients.rb
@@ -0,0 +1,119 @@
+module CSVImport
+  require "csv"
+
+  class SitesWithClients
+    include ActiveModel::Validations
+
+    SHARED_SECRET_BYTES = 10
+
+    attr_accessor :records
+
+    def initialize(csv)
+      @csv = remove_utf8_byte_order_mark(csv) if csv
+      @records = []
+
+      @records = parse_csv(@csv)
+    end
+
+    def save
+      return false unless valid?
+
+      sites_to_save = []
+      clients_to_save = []
+      fallback_policies_to_save = []
+      fallback_policy_responses_to_save = []
+      site_policies_to_save = []
+
+      @records.each do |site|
+        sites_to_save << { id: site.id, name: site.name, tag: site.name.parameterize(separator: "_"), policy_count: site.policies.to_a.count }
+
+        site.clients.each do |client|
+          client.valid?
+          clients_to_save << { ip_range: client.ip_range, shared_secret: client.shared_secret, site_id: site.id, radsec: client.radsec }
+        end
+
+        site.policies.each do |policy|
+          if policy.fallback?
+            fallback_policies_to_save << {
+              name: policy.name,
+              description: policy.description,
+              fallback: policy.fallback,
+              rule_count: policy.rule_count,
+              site_count: 1,
+            }
+
+            policy.responses.each do |response|
+              fallback_policy_responses_to_save << { policy_id: policy.id, response_attribute: response.response_attribute, value: response.value }
+            end
+          end
+
+          site_policies_to_save << { site_id: site.id, policy_id: policy.id }
+        end
+      end
+
+      ActiveRecord::Base.transaction do
+        Site.insert_all(sites_to_save)
+        Client.insert_all(clients_to_save)
+        Policy.insert_all(fallback_policies_to_save)
+        PolicyResponse.insert_all(fallback_policy_responses_to_save)
+        SitePolicy.insert_all(site_policies_to_save)
+      end
+    end
+
+  private
+
+    def parse_csv(csv)
+      id_of_last_site = Site.last&.id || 0
+      last_policy_id = Policy.last&.id || 0
+
+      CSV.parse(csv, headers: true).map.with_index(1) do |row, i|
+        site_name = row["Site Name"]
+        eap_clients = row["EAP Clients"]
+        radsec_clients = row["RadSec Clients"]
+        policies = row["Policies"]
+        fallback_policy = row["Fallback Policy"]
+
+        record = Site.new(
+          id: id_of_last_site + i,
+          name: site_name,
+        )
+
+        record.policies << Policy.new(
+          id: last_policy_id + i,
+          name: "Fallback policy for #{site_name}",
+          description: "Default fallback policy for #{site_name}",
+          fallback: true,
+        )
+
+        record.policies.first.responses << unwrap_responses(fallback_policy)
+
+        policies.split(";").each do |policy|
+          record.policies << Policy.find_by(name: policy)
+        end
+
+        eap_clients.split(";").each do |eap_client|
+          record.clients << Client.new(ip_range: eap_client, radsec: false, shared_secret: SecureRandom.hex(SHARED_SECRET_BYTES).upcase)
+        end
+
+        radsec_clients.split(";").each do |radsec_client|
+          record.clients << Client.new(ip_range: radsec_client, radsec: true, shared_secret: SecureRandom.hex(SHARED_SECRET_BYTES).upcase)
+        end
+
+        record
+      end
+    end
+
+    def remove_utf8_byte_order_mark(content)
+      return content[3..] if "\xEF\xBB\xBFA".force_encoding("ASCII-8BIT") == content[0..3]
+
+      content
+    end
+
+    def unwrap_responses(fallback_policy_responses)
+      fallback_policy_responses.to_s.split(";").map do |r|
+        response_attribute, value = r.split("=")
+        PolicyResponse.new(response_attribute: response_attribute, value: value)
+      end
+    end
+  end
+end
---
+++ spec/models/CSV_import/sites_clients_spec.rb
@@ -0,0 +1,63 @@
+require "rails_helper"
+
+describe CSVImport::SitesWithClients, type: :model do
+  subject { described_class.new(file_contents) }
+
+  context "valid csv entries" do
+    let(:file_contents) do
+      "Site Name,EAP Clients,RadSec Clients,Policies,Fallback Policy
+Petty France,128.0.0.1;10.0.0.1/32,128.0.0.1,Test Policy 1;Test Policy 2,Dlink-VLAN-ID=888;Reply-Message=hi"
+    end
+
+    before do
+      create(:policy, name: "Test Policy 1")
+      create(:policy, name: "Test Policy 2")
+    end
+
+    it "creates valid sites and clients" do
+      expect(subject).to be_valid
+      expect(subject.errors).to be_empty
+      expect(subject.records.count).to be(1)
+
+      expected_site = subject.records.first
+      expect(expected_site.name).to eq("Petty France")
+      expect(expected_site.policies.first.name).to eq("Fallback policy for Petty France")
+      expect(expected_site.policies.second.name).to eq("Test Policy 1")
+      expect(expected_site.policies.last.name).to eq("Test Policy 2")
+
+      expect(expected_site.clients.first.ip_range).to eq("128.0.0.1")
+      expect(expected_site.clients.first.shared_secret).to_not be_nil
+      expect(expected_site.clients.first.radsec).to be_falsey
+
+      expect(expected_site.clients.last.ip_range).to eq("128.0.0.1")
+      expect(expected_site.clients.last.shared_secret).to_not be_nil
+      expect(expected_site.clients.last.radsec).to be true
+
+      expect(expected_site.policies.first.responses.first.response_attribute).to eq("Dlink-VLAN-ID")
+      expect(expected_site.policies.first.responses.first.value).to eq("888")
+
+      expect(expected_site.policies.first.responses.second.response_attribute).to eq("Reply-Message")
+      expect(expected_site.policies.first.responses.second.value).to eq("hi")
+
+      expect(subject.save).to be_truthy
+
+      saved_site = Site.last
+
+      expect(saved_site.id).to_not be_nil
+      expect(saved_site.tag).to eq("petty_france")
+      expect(saved_site.clients.first.ip_range).to eq("128.0.0.1/32")
+      expect(saved_site.clients.last.ip_range).to eq("128.0.0.1/32")
+
+      expect(saved_site.policy_count).to eq(3)
+      expect(saved_site.fallback_policy.name).to eq("Fallback policy for Petty France")
+      expect(saved_site.fallback_policy.responses.first.response_attribute).to eq("Dlink-VLAN-ID")
+      expect(saved_site.fallback_policy.responses.first.value).to eq("888")
+      expect(saved_site.fallback_policy.responses.second.response_attribute).to eq("Reply-Message")
+      expect(saved_site.fallback_policy.responses.second.value).to eq("hi")
+
+      saved_site.clients.each do |client|
+        expect(client.id).to_not be_nil
+      end
+    end
+  end
+end
