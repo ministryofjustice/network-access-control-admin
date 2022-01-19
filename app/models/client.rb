016420015368592c79cf3a7d30009a204b362be8
csabagyorfi <csaba@madetech.com>
2022-01-19 14:50:25 +0000

    Add debug logs around client creation

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,11 +3,16 @@ class Client < ApplicationRecord
 
   validates_presence_of :ip_range, :shared_secret
   validate :validate_ip, on: %i[create update]
+  validate :ip_range_validation_start
   validates :ip_range, presence: true, uniqueness: { scope: :radsec }
+  validate :ip_range_validation_completed
 
 private
 
   def validate_ip
+    start_time = Time.now
+    pp "IP validation started"
+
     return if ip_range.blank?
 
     unless IPAddress.valid_ipv4_subnet?(ip_range) || IPAddress.valid_ipv4?(ip_range)
@@ -23,6 +28,16 @@ private
 
       return errors.add(:ip_range, "IP overlaps with #{client.site.name} - #{client.ip_range}")
     end
+
+    pp "IP validation completed in #{Time.now - start_time}"
+  end
+
+  def ip_range_validation_start
+    @start_time = Time.now
+  end
+
+  def ip_range_validation_completed
+    pp "IP range uniqness validation completed in #{Time.now - @start_time}"
   end
 
   audited
