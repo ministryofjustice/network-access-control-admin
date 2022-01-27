73b54e53be91559c23dee6cf25b87a3fc36f45b8
csabagyorfi <csaba@madetech.com>
2022-01-27 17:16:19 +0000

    Allow importing sites with no clients

--- spec/models/CSV_import/sites_with_clients_spec.rb
+++ spec/models/CSV_import/sites_with_clients_spec.rb
@@ -43,6 +43,36 @@ Petty France,128.0.0.1;10.0.0.1/32,128.0.0.1,Test Policy 1;Test Policy 2,Dlink-V
     end
   end
 
+  context "valid csv entries without clients or policies" do
+    let(:file_contents) do
+      "Site Name,EAP Clients,RadSec Clients,Policies,Fallback Policy
+Petty France,,,,Dlink-VLAN-ID=888;Reply-Message=hi"
+    end
+
+    let(:parse_sites_with_clients) { UseCases::CSVImport::ParseSitesWithClients.new(file_contents) }
+
+    it "creates valid sites and fallback policies" do
+      expect(subject).to be_valid
+      expect(subject.errors).to be_empty
+      expect(subject.records.count).to be(1)
+
+      expect(subject.save).to be_truthy
+
+      saved_site = Site.last
+
+      expect(saved_site.id).to_not be_nil
+      expect(saved_site.tag).to eq("petty_france")
+      expect(saved_site.clients).to be_empty
+
+      expect(saved_site.policy_count).to eq(1)
+      expect(saved_site.fallback_policy.name).to eq("Fallback policy for Petty France")
+      expect(saved_site.fallback_policy.responses.first.response_attribute).to eq("Dlink-VLAN-ID")
+      expect(saved_site.fallback_policy.responses.first.value).to eq("888")
+      expect(saved_site.fallback_policy.responses.second.response_attribute).to eq("Reply-Message")
+      expect(saved_site.fallback_policy.responses.second.value).to eq("hi")
+    end
+  end
+
   context "when CSV parser return errors" do
     let(:file_contents) { "INVALID" }
     let(:parse_sites_with_clients) { instance_double(UseCases::CSVImport::ParseSitesWithClients) }
