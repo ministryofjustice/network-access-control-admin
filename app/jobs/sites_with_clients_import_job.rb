0bb83cbd0e529c3243cd6c210678164c28937462
csabagyorfi <csaba@madetech.com>
2022-02-07 15:04:05 +0000

    Revert "Add debug logs to test sites import"
    This reverts commit b1e63f2391270e2ee680b8bd1daa4a90e544fac8.

--- app/jobs/sites_with_clients_import_job.rb
+++ app/jobs/sites_with_clients_import_job.rb
@@ -1,8 +1,6 @@
 class SitesWithClientsImportJob < ActiveJob::Base
   def perform(contents, csv_import_result, user)
     Audited.audit_class.as_user(user) do
-      start_time = Time.now
-      pp "Start importing sites #{start_time}"
       result = UseCases::CSVImport::SitesWithClients.new(contents).save
 
       if result.fetch(:errors).any?
@@ -12,7 +10,6 @@ class SitesWithClientsImportJob < ActiveJob::Base
         deploy_service
       end
 
-      pp "Completed importing sites in #{Time.now - start_time}"
       csv_import_result.update!(completed_at: Time.now)
     end
   end
