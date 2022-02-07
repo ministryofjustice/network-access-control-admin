96d8dcd0514a20a4c40ee8349782a37830773ead
csabagyorfi <csaba@madetech.com>
2022-02-07 10:06:06 +0000

    Add breadcrumbs navigation to sites imports

--- app/controllers/sites_imports_controller.rb
+++ app/controllers/sites_imports_controller.rb
@@ -1,4 +1,7 @@
 class SitesImportsController < ApplicationController
+  before_action :set_sites_crumbs, only: %i[new show]
+  before_action :set_sites_imports_crumbs, only: %i[show]
+
   def new
     @sites_with_clients_import = UseCases::CSVImport::SitesWithClients.new
 
@@ -16,5 +19,15 @@ class SitesImportsController < ApplicationController
 
   def show
     @csv_import_result = CsvImportResult.find(params.fetch(:id))
+  end
+
+private
+
+  def set_sites_crumbs
+    @navigation_crumbs << ["Sites", sites_path]
+  end
+
+  def set_sites_imports_crumbs
+    @navigation_crumbs << ["Sites with Clients Imports", new_sites_import_path]
   end
 end
