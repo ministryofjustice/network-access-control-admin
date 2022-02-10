a01539b10ac19ab9b24391193c562ccc1b768c6a
Emile Swarts <emile.swarts@digital.justice.gov.uk>
2022-02-10 11:51:11 +0000

    Create 3 responses per MAB entry (#374)
    * Create 3 responses per MAB entry
    
    This will create responses in raw sql to sidestep slow validations
    
    * Commit changes made by Rubocop
    
    Co-authored-by: Rubocop <runner@fv-az292-177.tqgtyn10aqxuxoi0mkfbldi5vh.jx.internal.cloudapp.net>

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,28 +52,28 @@ end
 #   site.policies << Policy.where(fallback: false).select(:id).sample(5)
 # end
 
-p "creating MABs"
-100_000.times do |m|
-  MacAuthenticationBypass.create!(
-    address: mac_address.to_s,
-    name: "Performance MAB#{m}",
-    description: "MAC Address for #{m}",
-    # responses: [
-    #   MabResponse.create!(
-    #     response_attribute: "Tunnel-Type",
-    #     value: "VLAN"
-    #   ),
-    #   MabResponse.create!(
-    #     response_attribute: "Tunnel-Medium-Type",
-    #     value: "IEEE-802"
-    #   ),
-    #   MabResponse.create!(
-    #     response_attribute: "Tunnel-Private-Group-Id",
-    #     value: "777"
-    #   ),
-    # ]
-  )
-end
+# p "creating MABs"
+# 100_000.times do |m|
+# MacAuthenticationBypass.create!(
+#   address: "aa-bb-cc-dd-ee-ff",
+#   name: "Performance",
+#   description: "MAC Address ",
+# responses: [
+#   MabResponse.create!(
+#     response_attribute: "Tunnel-Type",
+#     value: "VLAN"
+#   ),
+#   MabResponse.create!(
+#     response_attribute: "Tunnel-Medium-Type",
+#     value: "IEEE-802"
+#   ),
+#   MabResponse.create!(
+#     response_attribute: "Tunnel-Private-Group-Id",
+#     value: "777"
+#   ),
+# ]
+#   )
+# end
 
 # p "creating certificates"
 # 10.times do |c|
@@ -86,3 +86,16 @@ end
 #     filename: "#{c}.pem",
 #   )
 # end
+
+MacAuthenticationBypass.all.each do |mab|
+  next unless mab.responses.empty?
+
+  sql = "insert into responses(response_attribute, value, mac_authentication_bypass_id) VALUES ('Tunnel-Type', 'VLAN', #{mab.id})"
+  ActiveRecord::Base.connection.execute(sql)
+
+  sql = "insert into responses(response_attribute, value, mac_authentication_bypass_id) VALUES ('Tunnel-Medium-Type', 'IEEE-802', #{mab.id})"
+  ActiveRecord::Base.connection.execute(sql)
+
+  sql = "insert into responses(response_attribute, value, mac_authentication_bypass_id) VALUES ('Tunnel-Private-Group-Id', '777', #{mab.id})"
+  ActiveRecord::Base.connection.execute(sql)
+end
