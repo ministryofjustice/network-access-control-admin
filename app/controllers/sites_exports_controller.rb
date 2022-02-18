99064da66ff442fb042c966ad36135223f4bd708
csabagyorfi <csaba@madetech.com>
2022-02-18 12:59:50 +0000

    Allow downloading Sites with Clients CSV

--- app/controllers/sites_exports_controller.rb
+++ app/controllers/sites_exports_controller.rb
@@ -1,9 +1,12 @@
 class SitesExportsController < ApplicationController
   def new
-    @sites_with_clients_export = UseCases::CSVExport::SitesWithClients.new
+    sites_with_clients_export = UseCases::CSVExport::SitesWithClients.new
 
-    authorize! :create, @sites_with_clients_export
+    authorize! :create, sites_with_clients_export
   end
 
-  def create; end
+  def create
+    sites_with_clients_export = UseCases::CSVExport::SitesWithClients.new.call
+    send_data sites_with_clients_export, filename: "sites_with_clients.csv", type: "application/csv"
+  end
 end
