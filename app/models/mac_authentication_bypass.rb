eda1fc054e73c9f868c692fa0d3a0ad838c5dc06
csabagyorfi <csaba@madetech.com>
2022-02-24 10:41:40 +0000

    Validate the presence of MAB name and description

--- app/models/mac_authentication_bypass.rb
+++ app/models/mac_authentication_bypass.rb
@@ -3,7 +3,7 @@ class MacAuthenticationBypass < ApplicationRecord
 
   paginates_per 50
 
-  validates :address, presence: true
+  validates :address, :name, :description, presence: true
   validates :address, uniqueness: true, unless: :skip_uniqueness_validation?
   validates_format_of :address, with: /\A([0-9a-f]{2}-){5}([0-9a-f]{2})\z/, on: :create, if: -> { address.present? }
 
