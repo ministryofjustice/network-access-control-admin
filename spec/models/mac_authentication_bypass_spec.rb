80c793160cbdcf0e0b12ce17a92305e3f7a1f3ec
csabagyorfi <csaba@madetech.com>
2022-02-24 10:41:40 +0000

    Validate the presence of MAB name and description

--- spec/models/mac_authentication_bypass_spec.rb
+++ spec/models/mac_authentication_bypass_spec.rb
@@ -9,6 +9,8 @@ describe MacAuthenticationBypass, type: :model do
 
   it { is_expected.to belong_to(:site).optional }
   it { is_expected.to validate_presence_of :address }
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :description }
   it { is_expected.to validate_uniqueness_of(:address).case_insensitive }
 
   it "validates the format of the MAC addresses" do
