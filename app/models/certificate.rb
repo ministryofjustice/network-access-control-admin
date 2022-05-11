8d74116a06c845760a3b0fcf0d0383c3690c93ba
Yusuf <yusuf@madetech.com>
2022-05-11 16:38:34 +0100

    Update model specs to generate server certificate contents

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -7,9 +7,20 @@ class Certificate < ApplicationRecord
   validates_inclusion_of :category, in: %w[EAP RADSEC]
   validates_uniqueness_of :filename, scope: :category
   validates_format_of :filename, with: /\A.+(.pem)+\z/
+  validate :validate_server_certificate
 
   def server_certificate?
     filename == "server.pem"
+  end
+
+  def validate_server_certificate
+    return unless server_certificate?
+
+    results = UseCases::ValidateServerCertificate.new(certificate: contents).call
+
+    results.each do |result|
+      errors.add :base, result
+    end
   end
 
 private
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -49,7 +49,7 @@ RSpec.configure do |config|
 
   config.include AuditLoggingFeatureHelpers, type: :feature
   config.include DeploymentFeatureHelpers, type: :feature
-  config.include CreateCertificateHelpers, type: :feature
+  config.include CreateCertificateHelpers
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
