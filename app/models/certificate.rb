8851ed032e5cbdcbd8d8e470c0f836164efa3a5f
Yusuf <yusuf@madetech.com>
2022-05-17 15:49:40 +0100

    Add validation for private key to match certificate

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -7,7 +7,7 @@ class Certificate < ApplicationRecord
   validates_inclusion_of :category, in: %w[EAP RADSEC]
   validates_uniqueness_of :filename, scope: :category
   validates_format_of :filename, with: /\A.+(.pem)+\z/
-  validate :validate_server_certificate
+  validate :validate_server_certificate, on: :create
 
   def server_certificate?
     filename == "server.pem"
@@ -16,11 +16,12 @@ class Certificate < ApplicationRecord
   def validate_server_certificate
     return unless server_certificate?
 
-    passphrase = if category == "EAP"
-      ENV.fetch("EAP_SERVER_PRIVATE_KEY_PASSPHRASE")
-    elsif category == "RADSEC"
-      ENV.fetch("RADSEC_SERVER_PRIVATE_KEY_PASSPHRASE")
-    end
+    passphrase = case category
+                 when "EAP"
+                   ENV.fetch("EAP_SERVER_PRIVATE_KEY_PASSPHRASE")
+                 when "RADSEC"
+                   ENV.fetch("RADSEC_SERVER_PRIVATE_KEY_PASSPHRASE")
+                 end
 
     validation_errors = UseCases::ValidateServerCertificate.new(certificate: contents).call(passphrase)
 
--- spec/use_cases/validate_server_certificate_spec.rb
+++ spec/use_cases/validate_server_certificate_spec.rb
@@ -14,7 +14,7 @@ describe UseCases::ValidateServerCertificate do
       )
     end
 
-    it "can handle invalid certificates" do
+    it "fails validation" do
       expect(use_case.call).to include("Certificate does not contain a valid private key")
     end
   end
@@ -32,8 +32,26 @@ describe UseCases::ValidateServerCertificate do
       )
     end
 
-    it "can handle incorrect passphrase" do
+    it "fails validation" do
       expect(use_case.call("invalidpassphrase")).to include("Certificate does not contain a valid private key")
+    end
+  end
+
+  context "when the private key does not match the certificate" do
+    let(:invalid_certificate_path) { "./spec/acceptance/certificate/dummy_certificate/server_certificate/invalid_certificate.pem" }
+
+    before do
+      File.open(invalid_certificate_path, "w") { |f| f.write(generate_self_signed_certificate.fetch(:cert_and_invalid_key)) }
+    end
+
+    subject(:use_case) do
+      described_class.new(
+        certificate: File.read(invalid_certificate_path),
+      )
+    end
+
+    it "fails validation" do
+      expect(use_case.call(ENV.fetch("EAP_SERVER_PRIVATE_KEY_PASSPHRASE"))).to include("Certificate does not contain a matching private key")
     end
   end
 end
