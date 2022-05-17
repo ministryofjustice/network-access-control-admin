7a836bb710d36cf58af2c64feafbb1e0d7387f60
Mitch Dawson <mitchell.dawson@justice.gov.uk>
2022-05-17 12:59:53 +0100

    WIP commit

--- spec/use_cases/validate_server_certificate_spec.rb
+++ spec/use_cases/validate_server_certificate_spec.rb
@@ -18,4 +18,23 @@ describe UseCases::ValidateServerCertificate do
       expect(use_case.call).to include("Certificate is missing a private key")
     end
   end
+
+  context "when the passphrase does not match" do
+
+    let(:valid_certificate_path) { "./spec/acceptance/certificate/dummy_certificate/server_certificate/valid_certificate.pem" }
+
+    before do
+      File.open(valid_certificate_path, "w") { |f| f.write(generate_self_signed_certificate.fetch(:cert_and_key)) }
+    end
+
+    subject(:use_case) do
+      described_class.new(
+        certificate: File.read(valid_certificate_path),
+      )
+    end
+
+    it "can handle incorrect passphrase" do
+      expect(use_case.call).to include("Certificate passphrase does not match")
+    end
+  end
 end
