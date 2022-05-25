52ca74b89b03cdd39fc013d0574315154c8a80a8
Emile <emile.swarts@digital.justice.gov.uk>
2022-05-25 18:41:30 +0100

    Ensure MAC addresses belong to sites in production

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -24,10 +24,10 @@ seed() {
 }
 
 main() {
-  if [ "$ENV" = "pre-production" ] || [ "$ENV" = "development" ]; then
+  # if [ "$ENV" = "production" ] || [ "$ENV" = "development" ]; then
     assume_deploy_role
     seed
-  fi
+  # fi
 }
 
 main
