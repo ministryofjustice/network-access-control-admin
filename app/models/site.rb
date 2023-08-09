1652db08c41aa4f1ce4ac77829ebb1051c56337d
Satish Gummadelli <satish.gummadelli@justice.gov.uk>
2023-08-09 05:36:59 +0100

    updates

--- app/models/site.rb
+++ app/models/site.rb
@@ -59,11 +59,11 @@ class Site < ApplicationRecord
     private
 
     def ransackable_associations(_auth_object = nil)
-      ["audits", "clients", "mac_authentication_bypasses", "policies", "site_policy"]
+      %w(audits clients mac_authentication_bypasses policies site_policy)
     end
 
     def ransackable_attributes(_auth_object = nil)
-      ["created_at", "id", "name", "policy_count", "tag", "updated_at"]
+      %w(created_at id name policy_count tag updated_at)
     end
   end
 end
