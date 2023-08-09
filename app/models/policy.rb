7bb90c44fb0899c3ce1b262068bfd25c3607ed81
Satish Gummadelli <satish.gummadelli@justice.gov.uk>
2023-08-09 05:48:19 +0100

    updates

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -29,10 +29,10 @@ private
   end
 
   def self.ransackable_attributes(_auth_object = nil)
-    ["action", "created_at", "description", "fallback", "id", "name", "rule_count", "site_count", "updated_at"]
+    %w[action created_at description fallback id name rule_count site_count updated_at]
   end
 
   def self.ransackable_associations(_auth_object = nil)
-    ["audits", "responses", "rules", "site_policy", "sites"]
+    %w[audits responses rules site_policy sites]
   end
 end
