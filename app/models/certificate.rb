0bfc5b549616c15bea89a6a22718bde2550ee88c
Satish Gummadelli <satish.gummadelli@justice.gov.uk>
2023-08-09 07:08:41 +0100

    updates

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -38,8 +38,11 @@ private
     end
   end
 
-  def self.ransackable_attributes(auth_object = nil)
-    ["category", "contents", "created_at", "description", "expiry_date", "extensions", "filename", "id", "issuer", "name", "serial", "subject", "updated_at"]
+  class << self
+    private
+    def ransackable_attributes(auth_object = nil)
+      ["category", "contents", "created_at", "description", "expiry_date", "extensions", "filename", "id", "issuer", "name", "serial", "subject", "updated_at"]
+    end
   end
 
   audited
