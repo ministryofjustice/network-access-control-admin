9342c9257221338b8a88ac4cb4faacacee4a7a4d
Sandhya Buddharaju <sandhya.buddharaju@justice.gov.uk>
2025-05-19 17:31:28 +0100

    removed the sample secret key to comply with gileaks check

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = ''
+  # config.secret_key = '<your own secret key>'
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
